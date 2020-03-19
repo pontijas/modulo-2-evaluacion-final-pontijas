e0a86337938ea94f42c3584940ddb41c5a8e275b
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-19 19:18:30 +0100

    set variables for button and input, and function that listens to this button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
+const serieInput = document.querySelector('.js-input');
+const btn = document.querySelector('.js-btn');
 
-console.log('holiii');
+function getSerieName(ev) {
+  ev.preventDefault();
+  console.log(serieInput.value);
+  fetch(`http://api.tvmaze.com/search/shows?q=${serieInput.value}`)
+    .then(response => response.json())
+    .then(dataSerie => dataSerie);
+  // devueve un array vacío
+  console.log(dataSerie);
+}
+
+btn.addEventListener('click', getSerieName);
