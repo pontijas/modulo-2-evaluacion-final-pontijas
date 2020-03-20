3e95f8334cc420a5d66fb547ce6ab643f549320e
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-20 13:23:22 +0100

    improvements

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,13 +5,15 @@ const serieInfo = document.querySelector('.js-ul');
 
 function getSerieName(ev) {
   ev.preventDefault();
-  console.log(serieInput.value);
+  // console.log(serieInput.value);
   fetch(`http://api.tvmaze.com/singlesearch/shows?q=${serieInput.value}`)
     .then(response => response.json())
     .then(dataSerie => {
-      serieInfo.innerHTML += `<li>${dataSerie.name}</li>`;
+      serieInfo.innerHTML += `<li><h2>${dataSerie.name}</h2></li>`;
       serieInfo.innerHTML += `<li><img src="${dataSerie.image.medium}" alt="póster de la serie"></li>`;
       console.log('holiii');
+      serieInfo.innerHTML += `<li><h3>${dataSerie.genres}</h3></li>`;
+      serieInfo.innerHTML += `<li><p>${dataSerie.summary}</p></li>`;
     });
 }
 
