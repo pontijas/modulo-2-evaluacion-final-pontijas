afb322f9a445dc2497f2f0786f9104bbc3054390
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-20 13:11:28 +0100

    clean code

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,8 +2,6 @@
 const serieInput = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
 const serieInfo = document.querySelector('.js-ul');
-const serieTitle = document.querySelector('.js-li_title');
-const serieImg = document.querySelector('.js-li_img');
 
 function getSerieName(ev) {
   ev.preventDefault();
@@ -14,11 +12,6 @@ function getSerieName(ev) {
       serieInfo.innerHTML += `<li>${dataSerie.name}</li>`;
       serieInfo.innerHTML += `<li><img src="${dataSerie.image.medium}" alt="póster de la serie"></li>`;
       console.log('holiii');
-
-      // estructura para recorrer array en el caso de que la petición a la APOi estuviera estructurada con un array
-      // for (let i = 0; i < dataArr.length; i++) {
-      //   serieTitle.innerHTML = dataSerie.dataArr[i].name;
-      // }
     });
 }
 
