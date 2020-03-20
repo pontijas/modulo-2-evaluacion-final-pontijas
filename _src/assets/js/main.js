d049bebcf1d8b859dfb5ba0d294dd37d32d00ee3
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-20 18:17:57 +0100

    change semantics in html, then change css and js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,27 +1,22 @@
 'use strict';
-const serieInput = document.querySelector('.js-input');
-const btn = document.querySelector('.js-btn');
-const serieInfo = document.querySelector('.js-ul');
+
+// generic variables
+
+const serieInput = document.querySelector('.js-search-input');
+const btnSearch = document.querySelector('.js-search-btn');
+const seriesDisplay = document.querySelector('.js-search-display');
+const favSeriesList = document.querySelector('.js-fav-list');
+
+// arrays
+
 let series = [];
 let favSeries = [];
 
-// single search
-// function getData(ev) {
-//   ev.preventDefault();
-//   // console.log(serieInput.value);
-//   fetch(`http://api.tvmaze.com/singlesearch/shows?q=${serieInput.value}`)
-//     .then(response => response.json())
-//     .then(dataSerie => {
-//       serieInfo.innerHTML += `<li><h2>${dataSerie.name}</h2></li>`;
-//       serieInfo.innerHTML += `<li><img src="${dataSerie.image.medium}" alt="póster de la serie"></li>`;
-//       serieInfo.innerHTML += `<li><h3>${dataSerie.genres}</h3></li>`;
-//       serieInfo.innerHTML += `<li><p>${dataSerie.summary}</p></li>`;
-//     });
-// }
+// functions
 
 function getData(ev) {
   ev.preventDefault();
-  console.log(serieInput.value);
+  // console.log(serieInput.value);
   fetch(`http://api.tvmaze.com/search/shows?q=${serieInput.value}`)
     .then(response => response.json())
     .then(dataSerie => {
@@ -33,11 +28,21 @@ function getData(ev) {
 function paintData() {
   for (const serie of series) {
     let htmlCode = '';
-    htmlCode += `<li class="css-li_serie"><h2>${serie.show.name}</h2>`;
+    htmlCode += `<article class="css-card">`;
+    htmlCode += `<h2>${serie.show.name}</h2>`;
     htmlCode += `<img src="${serie.show.image.medium}" alt="póster de la serie"`;
-    htmlCode += `</li>`;
-    serieInfo.innerHTML += htmlCode;
+    htmlCode += `</article>`;
+    seriesDisplay.innerHTML += htmlCode;
   }
 }
 
-btn.addEventListener('click', getData);
+btnSearch.addEventListener('click', getData);
+
+// add to favorites
+const liSerie = document.querySelector('.js-li-serie');
+
+function addToFavorites() {
+  console.log('me han clickado');
+}
+
+liSerie.addEventListener('click', addToFavorites);
