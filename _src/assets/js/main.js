cc1368d532b09ec011067c60aa40dae4722e9dda
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-20 12:19:49 +0100

    set fetch to API, get the image

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 const serieInput = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
+const serieInfo = document.querySelector('.js-ul');
 const serieTitle = document.querySelector('.js-li_title');
 const serieImg = document.querySelector('.js-li_img');
 
@@ -10,8 +11,8 @@ function getSerieName(ev) {
   fetch(`http://api.tvmaze.com/singlesearch/shows?q=${serieInput.value}`)
     .then(response => response.json())
     .then(dataSerie => {
-      serieTitle.innerHTML = dataSerie.name;
-      serieImg.innerHTML = dataSerie.language;
+      serieInfo.innerHTML += `<li>${dataSerie.name}</li>`;
+      serieInfo.innerHTML += `<li><img src="${dataSerie.img}" alt="póster de la serie"></li>`;
       console.log('holiii');
 
       // estructura para recorrer array en el caso de que la petición a la APOi estuviera estructurada con un array
