b970aee8e9649203ac0aab7a0b106640c74f2b4a
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-20 11:25:41 +0100

    set API request

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,15 +1,24 @@
 'use strict';
 const serieInput = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
+const serieTitle = document.querySelector('.js-li_title');
+const serieImg = document.querySelector('.js-li_img');
 
 function getSerieName(ev) {
   ev.preventDefault();
   console.log(serieInput.value);
-  fetch(`http://api.tvmaze.com/search/shows?q=${serieInput.value}`)
+  fetch(`http://api.tvmaze.com/singlesearch/shows?q=${serieInput.value}`)
     .then(response => response.json())
-    .then(dataSerie => dataSerie);
-  // devueve un array vacío
-  console.log(dataSerie);
+    .then(dataSerie => {
+      serieTitle.innerHTML = dataSerie.name;
+      serieImg.innerHTML = dataSerie.language;
+      console.log('holiii');
+
+      // estructura para recorrer array en el caso de que la petición a la APOi estuviera estructurada con un array
+      // for (let i = 0; i < dataArr.length; i++) {
+      //   serieTitle.innerHTML = dataSerie.dataArr[i].name;
+      // }
+    });
 }
 
 btn.addEventListener('click', getSerieName);
