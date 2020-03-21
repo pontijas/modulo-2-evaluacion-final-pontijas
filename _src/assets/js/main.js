d8958f6839d01ae67d074cb14ce7067ee3a89577
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-21 18:22:12 +0100

    get data from api and put request into get data function when clicking on search

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,47 +1,20 @@
 'use strict';
 
-// generic variables
+// http://api.tvmaze.com/search/shows?q=
+
+// get data
 
 const serieInput = document.querySelector('.js-search-input');
-const btnSearch = document.querySelector('.js-search-btn');
-const seriesDisplay = document.querySelector('.js-search-display');
-const favSeriesList = document.querySelector('.js-fav-list');
-
-// arrays
-
-let series = [];
-let favSeries = [];
-
-// get data from API
+const searchBtn = document.querySelector('.js-search-btn');
 
 function getData(ev) {
   ev.preventDefault();
-  console.log(serieInput.value);
-  fetch(`http://api.tvmaze.com/search/shows?q=${serieInput.value}`)
+  console.log('holiii');
+  fetch(`http://api.tvmaze.com/singlesearch/shows?q=friends`)
     .then(response => response.json())
     .then(dataSerie => {
-      series = dataSerie;
+      console.log(dataSerie);
     });
-  dataCode();
 }
 
-// paint data
-
-function dataCode() {
-  for (const serie of series) {
-    let htmlCode = '';
-    htmlCode += `<article class="css-card js-serie-card">`;
-    htmlCode += `<h2>${serie.show.name}</h2>`;
-    htmlCode += `<img src="${serie.show.image.medium}" alt="póster de la serie"`;
-    htmlCode += `</article>`;
-    htmlCode.innerHTML = seriesDisplay;
-  }
-}
-
-function paintData() {
-  for (let serie = 0; serie < series.length; serie++) {
-    htmlCode.innerHTML = series[0];
-  }
-}
-
-btnSearch.addEventListener('click', getData);
+searchBtn.addEventListener('click', getData);
