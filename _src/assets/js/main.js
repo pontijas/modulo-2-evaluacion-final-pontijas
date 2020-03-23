99b1393a62471a5f58cdc7082f581f39feccaaba
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-23 09:56:37 +0100

    clean code in master

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,7 @@
 const serieInput = document.querySelector('.js-search-input');
 const searchBtn = document.querySelector('.js-search-btn');
 const seriesDisplay = document.querySelector('.js-search-display');
+const favList = document.querySelector('.js-fav-list');
 
 // arrays
 
@@ -18,7 +19,6 @@ function getData(ev) {
   fetch(` http://api.tvmaze.com/search/shows?q=${serieInput.value}`)
     .then(response => response.json())
     .then(dataSerie => {
-      // guardamos los datos en un array
       series = dataSerie;
       paintCards();
     });
@@ -27,7 +27,6 @@ function getData(ev) {
 // paint data
 
 function setHtmlCode(serie) {
-  // esta función va a recorrer los objetos contenidos dentro de array series
   let htmlCode = '';
 
   if (serie.show.image === null) {
@@ -41,12 +40,10 @@ function setHtmlCode(serie) {
     htmlCode += `<img src="${serie.show.image.medium}" alt="póster de la película">`;
     htmlCode += `</article>`;
   }
-  // console.log(series);
   return htmlCode;
 }
 
 const paintCards = () => {
-  // esta función va a recorrer los objetos dentro del array series en el cual hay un objeto-serie- en cada posición-index-
   let seriesCode = '';
   for (const serie of series) {
     seriesCode += setHtmlCode(serie);
@@ -59,50 +56,29 @@ searchBtn.addEventListener('click', getData);
 
 // add to favorites
 
-// listen to buttons in previously generated htmlCode
-
 function listenAddFavBtn() {
   const addFavBtns = document.querySelectorAll('.js-add-fav');
-  // recorremos el array donde está la información del código generado, para poder trabajar con cada objeto individualmente
+
   for (const favBtn of addFavBtns) {
-    // le añadimos un add event listener a los botones creados
     favBtn.addEventListener('click', addFavSerie);
   }
 }
 
 function addFavSerie(event) {
-  // console.log('evento', event.currentTarget, event.currentTarget.dataset);
-  // hemos identificado el objeto clickado y nos quedamos con esa info
-  // console.log(event.currentTarget.dataset.id);
-  // guardamos la información del elemento clickado en una constante
   const clickedId = parseInt(event.currentTarget.dataset.id);
-  console.log('el id clickado es', clickedId);
-  // vamos a recorrer el array para quedarnos con los id que coincidan
-  // declaramos fav serie fuera del for porque lo queremos reutilizar después
+
   let favSerie;
   for (const serie of series) {
-    console.log('holiii', serie.show);
-    // serie.show me escribe todas las series guardadas en el arrays series, ahora necesito que se quede con la que tiene el id igual
     if (serie.show.id === clickedId) {
-      // si el id del serie.show coincide con clickedId entonces favSerie tiene el valor de serie.show
       favSerie = serie.show;
-      console.log('bingo', favSerie);
     }
   }
-  // ahora me interesa que los objetos favSerie los vaya metiendo en el array favSeries que declaré al principio
-  console.log('rebingo', favSerie);
-  // añadimos la serie seleccionada como favorita al array de favSeries
   favSeries.push(favSerie);
-
-  console.log('esto es favSerie', favSerie);
-  console.log('esto es favSeries el array que contiene cada favSerie', favSeries);
-
   paintFavSeriesList();
+  setInLocalStorage();
 }
 
 // paint favorite series list
-
-const favList = document.querySelector('.js-fav-list');
 
 function getFavListCode(favSerie) {
   let favListCode = '';
@@ -120,10 +96,7 @@ function getFavListCode(favSerie) {
     favListCode += `<button class="css-clear-btn">Quitar</button>`;
     favListCode += `</li>`;
   }
-  console.log('esto es favListCode', favListCode);
-
   return favListCode;
-  // (favList.innerHTML += favListCode);
 }
 
 function paintFavSeriesList() {
@@ -132,3 +105,22 @@ function paintFavSeriesList() {
     favList.innerHTML += getFavListCode(favSerie);
   }
 }
+
+// local storage
+
+function setInLocalStorage() {
+  const stringifyFavSeries = JSON.stringify(favSeries);
+  localStorage.setItem('favSeries', stringifyFavSeries);
+}
+
+function getFromLocalStorage() {
+  const storedData = localStorage.getItem('favSeries');
+  console.log('Esto es storedData', storedData);
+  // si el stored data es diferente de null (si es la primera vez que la usuaria arraca la pagina entonces es null, no hay info ahi)
+  if (storedData !== null) {
+    favSeries = JSON.parse(storedData);
+    paintCards();
+  }
+}
+
+getFromLocalStorage();
